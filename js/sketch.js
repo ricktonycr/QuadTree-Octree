53ad9a0f15fb6991b9c1dbf109723439a8e696d9
ddeza <ddezav@gmail.com>
2022-08-28 00:22:17 -0500

    exercise 6 complete

--- js/sketch.js
+++ js/sketch.js
@@ -1,3 +1,4 @@
+/* 
 let qt;
 let count = 0;
 function setup() {
@@ -15,6 +16,28 @@ function setup() {
         qt.insert(p);
     }
 
+    background(0);
+    qt.show();
+}
+ */
+
+let qt;
+let count = 0;
+
+function setup() {
+    createCanvas(400, 400);
+    let boundary = new Rectangle(200, 200, 200, 200);
+    qt = new QuadTree(boundary, 4);
+}
+
+function draw() {
+    background(0);
+    if (mouseIsPressed) {
+        for (let i = 0; i < 1; i++) {
+            let m = new Point(mouseX + random(-5, 5), mouseY + random(-5, 5));
+            qt.insert(m)
+        }
+    }
     background(0);
     qt.show();
 }
